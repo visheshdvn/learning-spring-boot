d663eeb89285ab7289eac0f04538f8923ff8de22
visheshdvn <vishesh.dh1@gmail.com>
2026-04-05 17:54:01 +0530

    logger configured

--- 7JobPortal/jobportal/src/main/java/com/eazybytes/jobportal/security/PathsConfig.java
+++ 7JobPortal/jobportal/src/main/java/com/eazybytes/jobportal/security/PathsConfig.java
@@ -17,6 +17,7 @@ public class PathsConfig
 				"/api/auth/register/public",
 				"/api/companies/public",
 				"/api/csrf-token/public",
+				"/api/logging/public",
 				"/api/swagger-ui.html",
 				"/swagger-ui/**",
 				"/api/v3/api-docs/**",
